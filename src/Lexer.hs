b61737b849ea6d8f4db3b3df73cc0aed2c0094ba
Dillon Huff <dillonhuff@gmail.com>
2014-03-30 11:20:02 -0700

    lexer no recognizes reserved words

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,7 +1,7 @@
 module Lexer(
 	tok, pos,
 	lexer,
-	Tok(I, Num, Op, Boolean)) where
+	Tok(I, Num, Op, Boolean, LET, EQUAL, IN, IF, THEN, ELSE)) where
 
 import ErrorHandling
 import Text.ParserCombinators.Parsec
@@ -30,7 +30,17 @@ data Tok
 	| Op String
 	| Num Int
 	| Boolean Bool
+	| LET
+	| EQUAL
+	| IN
+	| IF
+	| THEN
+	| ELSE
 	deriving (Eq, Show)
+
+resToTok =
+	[("let", LET), ("=", EQUAL), ("in", IN), ("if", IF)
+	,("then", THEN), ("else", ELSE)]
 
 lexer :: String -> ThrowsError [PosTok]
 lexer str = case parse pToks "TFL" str of
@@ -44,9 +54,43 @@ pToks = do
 
 pTok = do
 	t <- pNum
+		<|> pIdentOrRes
+		<|> pBool
 	return t
 
 pNum = do
 	pos <- getPosition
 	digs <- many1 digit
 	return $ PT (Num $ read digs) pos
+
+pIdentOrRes = do
+	pos <- getPosition
+	idOrRes <- pId
+		<|> pResName
+	return $ case lookup idOrRes resToTok of
+		Just t -> PT t pos
+		Nothing -> PT (I idOrRes) pos
+
+pId = do
+	startChar <- lower
+	rest <- many alphaNum
+	return (startChar:rest)
+
+pResName = do
+	rName <- string "="
+	return rName
+
+pBool = do
+	b <- pTrue
+		<|> pFalse
+	return b
+
+pTrue = do
+	pos <- getPosition
+	val <- string "True"
+	return $ PT (Boolean True) pos
+
+pFalse = do
+	pos <- getPosition
+	val <- string "False"
+	return $ PT (Boolean False) pos
--- test/LexerTests.hs
+++ test/LexerTests.hs
@@ -8,10 +8,50 @@ import Test.HUnit
 allLexerTests = runTestTT tests
 
 tests = TestList
-	[lexer_Number]
+	[lexer_Number
+	,lexer_Id
+	,lexer_True
+	,lexer_False
+	,lexer_let
+	,lexer_equal
+	,lexer_in
+	,lexer_if
+	,lexer_then
+	,lexer_else
+	,lexer_overlapResAndId]
 
 lexer_Number =
 	tokenTest "1239" [(Num 1239)]
+
+lexer_Id =
+	tokenTest "a" [(I "a")]
+
+lexer_True =
+	tokenTest "True" [(Boolean True)]
+
+lexer_False =
+	tokenTest "False" [(Boolean False)]
+
+lexer_let =
+	tokenTest "let" [LET]
+
+lexer_equal =
+	tokenTest "=" [EQUAL]
+
+lexer_in =
+	tokenTest "in" [IN]
+
+lexer_if =
+	tokenTest "if" [IF]
+
+lexer_then =
+	tokenTest "then" [THEN]
+
+lexer_else =
+	tokenTest "else" [ELSE]
+
+lexer_overlapResAndId =
+	tokenTest "letter" [(I "letter")]
 
 tokenTest input expected = TestCase
 	(assertEqual ("Input: " ++ show input)
